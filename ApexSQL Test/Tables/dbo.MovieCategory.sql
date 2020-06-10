602f37984f254bfbc70804b802731d2f881165a5
jsurretsky <>
2020-06-10 11:50:12 -0700

    Applied changes

--- ApexSQL Test/Tables/dbo.MovieCategory.sql
+++ ApexSQL Test/Tables/dbo.MovieCategory.sql
@@ -5,36 +5,8 @@ GO
 CREATE TABLE [dbo].[MovieCategory] (
 		[CategoryId]       [int] NOT NULL,
 		[CategoryName]     [varchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-		[RentalPrice]      [money] NOT NULL,
-		CONSTRAINT [MovieCategory_UK]
-		UNIQUE
-		NONCLUSTERED
-		([CategoryName])
-		ON [PRIMARY],
-		CONSTRAINT [MovieCategory_PK]
-		PRIMARY KEY
-		CLUSTERED
-		([CategoryId])
-	ON [PRIMARY]
+		[RentalPrice]      [money] NOT NULL
 )
-GO
-ALTER TABLE [dbo].[MovieCategory]
-	ADD
-	CONSTRAINT [CK__MovieCate__Categ__2F10007B]
-	CHECK
-	([CategoryId]>(0))
-GO
-ALTER TABLE [dbo].[MovieCategory]
-CHECK CONSTRAINT [CK__MovieCate__Categ__2F10007B]
-GO
-ALTER TABLE [dbo].[MovieCategory]
-	ADD
-	CONSTRAINT [CK__MovieCate__Renta__300424B4]
-	CHECK
-	([RentalPrice]>(0))
-GO
-ALTER TABLE [dbo].[MovieCategory]
-CHECK CONSTRAINT [CK__MovieCate__Renta__300424B4]
 GO
 ALTER TABLE [dbo].[MovieCategory] SET (LOCK_ESCALATION = TABLE)
 GO
